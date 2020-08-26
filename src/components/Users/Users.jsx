9f40b9b8ab90b1b2322772fe3c7b1cc8260db2c0
Alexander-PC <grishin057@gmail.com>
2020-08-26 23:33:49 +0300

    -add styles usersPage

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
 
 let Users = (props) => {
+   const html = props.users.map(u => <div key={u.id} className="user-friend">
+      <div className="user-friend__item">
 
+         <div className="user-friend__image"><img className="user-friend__img" src={u.photo} /></div>
+         {u.followed ?
+            <button className="user-friend__btn" onClick={() => { props.unfollow(u.id) }} > Unfollow </button>
+            : <button className="user-friend__btn" onClick={() => { props.follow(u.id) }} > Follow </button>
+         }
+      </div>
+
+      <div className="user-friend__info">
+
+         <div className="user-friend__info-main">
+            <div className="user-friend__name">{u.name} {u.surname}</div>
+            <div className="user-friend__status">{u.status}</div>
+         </div>
+
+         <div className="user-friend__info-submain">
+            <div className="user-friend__contry">{u.location.country}</div>
+            <div className="user-friend__city">{u.location.city}</div>
+         </div>
+
+      </div>
+
+   </div>
+   )
    return (
+      <div className="users-list">
+         {html}
 
-      props.users.map(u => <div key={u.id}>
-         u.name
-      </div>)
+      </div>
    )
 }
 
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -5,9 +5,9 @@ const SET_USERS = 'SET-USERS'
 
 let initialState = {
    users: [
-      { id: 1, name: "Alexander", surname: "Pavlov", status: "He-he", location: { country: "Russia", city: "Moscow" }, photo: "", followed: false },
-      { id: 2, name: "Maria", surname: "Kudelko", status: "wow i am here", location: { country: "Russia", city: "Moscow" }, photo: "", followed: true },
-      { id: 3, name: "Ivan", surname: "Komarov", status: "lets talk", location: { country: "Russia", city: "Orel" }, photo: "", followed: false },
+      { id: 1, name: "Alexander", surname: "Pavlov", status: "He-he", location: { country: "Russia", city: "Moscow" }, photo: "https://static4.depositphotos.com/1000605/297/i/450/depositphotos_2975699-stock-photo-style-beauty-guy.jpg", followed: false },
+      { id: 2, name: "Maria", surname: "Kudelko", status: "wow i am here", location: { country: "Russia", city: "Moscow" }, photo: "https://i.pinimg.com/originals/a3/c9/30/a3c9301d58ef886e3ce661da0875d87e.jpg", followed: true },
+      { id: 3, name: "Ivan", surname: "Komarov", status: "lets talk", location: { country: "Russia", city: "Orel" }, photo: "https://static4.depositphotos.com/1000605/297/i/450/depositphotos_2975699-stock-photo-style-beauty-guy.jpg", followed: false },
    ],
 }
 
